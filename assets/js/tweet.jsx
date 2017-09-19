6ffd5bdf253e7874c410f1ca75c557d6eac0f1a1
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-09-18 22:05:48 -0700

    working in-text links

--- assets/js/tweet.jsx
+++ assets/js/tweet.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+const strToLink = (string) => {
+  let href = "";
+  // add link to mention
+  if (string.startsWith('@')) {
+    const mention = string.slice(1);
+    href = "https://twitter.com/" + mention;
+    return <a href={href}>{string}</a>;
+  // add link to hashtag
+  } else if (string.startsWith('#')) {
+    const tag = string.slice(1);
+    href = "https://twitter.com/hashtag/" + tag;
+    return <a href={href}>{string}</a>;
+  // add link to url
+  } else if (string.startsWith('http')) {
+    return <a href={string}>{string}</a>;
+  }
+};
 
 const Tweet = ({ fields }) => {
   const { id, userName, screenName, text, profileImageUrl, createdAt } = fields;
+  const words = text.split(" ").map(word => strToLink(word));
   return (
     <li key={ id } className='tweet-item'>
       <div className='item-left'>
@@ -11,11 +29,11 @@ const Tweet = ({ fields }) => {
       <div className='item-right'>
         <div className='name-date'>
           <span>{ userName }</span>
-          <a>@{ screenName }</a>
+          { strToLink("@" + screenName) }
           <span>{ createdAt }</span>
         </div>
         <div className='text'>
-          <p>{ text }</p>
+          <p>{ words }</p>
         </div>
       </div>
     </li>
